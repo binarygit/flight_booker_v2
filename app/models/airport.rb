75c23d7b96cb5eebb9f6aeaf0af114c983064c0b
binarygit <inscription4@outlook.com>
2022-03-02 15:52:54 +0545

    create departure associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,6 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, class_name: 'Flight', foreign_key: :departure_id 
+
   before_save do
     self.airport_code.upcase!
   end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,3 @@
 class Flight < ApplicationRecord
-  belongs_to :airport
+  belongs_to :departure_airport, class_name: 'Airport', foreign_key: :departure_id
 end
