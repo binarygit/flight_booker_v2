0584c304dfbb097dcea078e472177f853b943db8
binarygit <inscription4@outlook.com>
2022-03-02 16:04:57 +0545

    create arrival associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,6 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: 'Flight', foreign_key: :departure_id 
+  has_many :arriving_flights, class_name: 'Flight',  foreign_key: :arrival_id
 
   before_save do
     self.airport_code.upcase!
