63ba24178f5806588498a9e4915772d3ae6ca874
binarygit <inscription4@outlook.com>
2022-03-03 19:18:47 +0545

    test whether first flight departs from ktm

--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'flight departs from KTM on March 3rd 2022' do
+    departure_airport = flights(:to_pkr).departure_airport.airport_code
+    departure_date = flights(:to_pkr).start_date
+     assert_equal 'KTM', departure_airport
+     assert_equal '2022-03-02'.to_date, departure_date
+  end
 end
