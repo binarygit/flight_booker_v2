d707d4afc69a73feac6071bd76aeb090d3191776
binarygit <inscription4@outlook.com>
2022-03-04 17:30:55 +0545

    create tests for both flights

--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,10 +1,25 @@
 require "test_helper"
 
 class FlightTest < ActiveSupport::TestCase
-  test 'flight departs from KTM on March 3rd 2022' do
-    departure_airport = flights(:to_pkr).departure_airport.airport_code
-    departure_date = flights(:to_pkr).start_date
+  test 'flight KTM to PKR on March 3rd 2022' do
+    flight = flights(:to_pkr)
+    departure_airport = flight.departure_airport.airport_code
+    arrival_airport = flight.arrival_airport.airport_code
+    departure_date = flight.start_date
+
      assert_equal 'KTM', departure_airport
+     assert_equal 'PKR', arrival_airport
      assert_equal '2022-03-02'.to_date, departure_date
+  end
+
+  test 'flight PKR to KTM on March 4th 2022' do
+    flight = flights(:to_ktm)
+    departure_airport = flight.departure_airport.airport_code
+    arrival_airport = flight.arrival_airport.airport_code
+    departure_date = flight.start_date
+
+     assert_equal 'PKR', departure_airport
+     assert_equal 'KTM', arrival_airport
+     assert_equal '2022-03-03'.to_date, departure_date
   end
 end
