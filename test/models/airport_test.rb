e1b9be67ed145a93d97d5af16d3afa8855eafd6e
binarygit <inscription4@outlook.com>
2022-03-04 17:43:08 +0545

    test association from airport side

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class AirportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'KTM airport has a single departure flight on March 2 2022' do
+    airport = airports(:ktm)
+    departing_flights = airport.departing_flights
+    departure_date = departing_flights.first.start_date
+    
+    assert_equal 1, departing_flights.length
+    assert_equal 'Thu, 02 Mar 2022'.to_date, departure_date
+  end
+
+  test 'KTM airport has a single arrival flight on March 3 2022' do
+    airport = airports(:ktm)
+    arriving_flights = airport.arriving_flights
+    arrival_date = arriving_flights.first.start_date
+    
+    assert_equal 1, arriving_flights.length
+    assert_equal 'Thu, 03 Mar 2022'.to_date, arrival_date
+  end
 end
