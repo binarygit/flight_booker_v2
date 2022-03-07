27be4b228e140422c964d705c6d2e9c76ff3fe9a
binarygit <inscription4@outlook.com>
2022-03-07 19:16:47 +0545

    test pkr airport associations

--- test/models/airport_test.rb
+++ test/models/airport_test.rb
@@ -18,4 +18,22 @@ class AirportTest < ActiveSupport::TestCase
     assert_equal 1, arriving_flights.length
     assert_equal 'Thu, 03 Mar 2022'.to_date, arrival_date
   end
+
+  test 'PKR airport has a single arrival flight on March 2 2022' do
+    airport = airports(:pkr)
+    arriving_flights = airport.arriving_flights
+    arrival_date = arriving_flights.first.start_date
+    
+    assert_equal 1, arriving_flights.length
+    assert_equal 'Thu, 02 Mar 2022'.to_date, arrival_date
+  end
+
+  test 'PKR airport has a single departure flight on March 3 2022' do
+    airport = airports(:pkr)
+    departing_flights = airport.departing_flights
+    departure_date = departing_flights.first.start_date
+    
+    assert_equal 1, departing_flights.length
+    assert_equal 'Thu, 03 Mar 2022'.to_date, departure_date
+  end
 end
