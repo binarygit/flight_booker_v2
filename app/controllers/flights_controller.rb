7739c9ca8f859bb3b6bfa8fb940bd39e33823b3e
binarygit <inscription4@outlook.com>
2022-03-08 08:05:22 +0545

    send all airports to view and strongify params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,11 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all
+  end
+
+  private
+
+  def airport_params
+    params.permit(:username, :departure_airport, :arrival_airport)
   end
 end
