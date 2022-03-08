837306bccc68af5d0dc0d7f558900ff66cabbcf7
binarygit <inscription4@outlook.com>
2022-03-08 14:13:55 +0545

    create a search method for flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,13 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport', foreign_key: :departure_id
   belongs_to :arrival_airport, class_name: 'Airport', foreign_key: :arrival_id
+
+  def self.search(search)
+    if search.has_key?(:departure_airport)
+      dep_airport = Airport.where(airport_code: search[:departure_airport])
+      arr_airport = Airport.where(airport_code: search[:arrival_airport])
+      Flight.where(departure_id: dep_airport)
+            .where(arrival_id: arr_airport)
+    end
+  end
 end
