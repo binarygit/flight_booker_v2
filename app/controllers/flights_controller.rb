39bd1c8b2ea472900d8db82f01694b39d424dfb1
binarygit <inscription4@outlook.com>
2022-03-09 16:57:21 +0545

    bugfix: strong params exception when using require
    I was using scope: with my form to group the data together and using
    params.require(:flight).permit(...)
    But when no form submition was made and the url was just '../' instead
    of '../..?..' it threw an exception that :flight was empty.
    Makes perfect sense when looking back. Spent 4 hours on this cheeky
    little thing. Onwards!!

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,12 +2,12 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all.map {|a| [a.airport_code, a.id] }
     @dates = Flight.all.map {|f| [f.start_date, f.start_date]}
-    #@flights = Flight.search(params)
+    @flights = Flight.search(flight_params.to_h)
   end
 
   private
 
-  def airport_params
-    params.permit(:departure_airport, :arrival_airport, :departure_date)
+  def flight_params
+    params.permit(:departure_id, :arrival_id, :start_date)
   end
 end
