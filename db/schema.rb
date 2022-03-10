d66da8d6bc6810ac1678251b1a6a5aeaf58346d9
binarygit <inscription4@outlook.com>
2022-03-10 17:39:24 +0545

    created passenger model and migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_101404) do
+ActiveRecord::Schema.define(version: 2022_03_10_115349) do
 
   create_table "airports", force: :cascade do |t|
     t.string "airport_code"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2022_03_02_101404) do
     t.integer "arrival_id", null: false
     t.index ["arrival_id"], name: "index_flights_on_arrival_id"
     t.index ["departure_id"], name: "index_flights_on_departure_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "flights", "airports", column: "arrival_id"
