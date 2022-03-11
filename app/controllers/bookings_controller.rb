412c5e6608857ba8599c3b4868fc9ce7e7a9c4e9
binarygit <inscription4@outlook.com>
2022-03-11 16:18:04 +0545

    generate passenger fields

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,12 @@
 class BookingsController < ApplicationController
   def new
+    #@booking = Booking.new
+    #params[:passenger_num].to_i.times { @booking.build_passenger }
+    @flight = Flight.new
+    params[:passenger_num].to_i.times { @flight.passengers.build }
   end
 
   def create
+    byebug
   end
 end
